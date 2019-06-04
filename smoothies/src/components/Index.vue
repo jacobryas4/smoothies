dc30287f8d3b57dd369a9f97bcee992c098cc243
Jacob Bryant <jacobryas4@gmail.com>
2019-06-04 16:18:51 -0400

    added dummy data

---
+++ smoothies/src/components/Index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="hello">
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Index',
+  data () {
+    return {
+      smoothies: [
+        { title: 'Ninja Brew', slug: 'ninja-brew', ingredients: ['bananas', 'coffee', 'milk'], id: '1' },
+        { title: 'Morning Mood', slug: 'morning-mood', ingredients: ['mango', 'lime', 'juice'], id: '2'}
+      ]
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
