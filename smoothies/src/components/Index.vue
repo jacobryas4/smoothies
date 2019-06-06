f45917935c8e1d0dff9aa0db65e68702b1220331
Jacob Bryant <jacobryas4@gmail.com>
2019-06-06 13:01:32 -0400

    adding cards

--- smoothies/src/components/Index.vue
+++ smoothies/src/components/Index.vue
@@ -1,6 +1,15 @@
 <template>
   <div class="index container">
-    <div class="card" v-for="smoothie in smoothies" :key=></div>
+    <div class="card" v-for="smoothie in smoothies" :key="smoothie.id">
+      <div class="card-content">
+        <h2 class="indigo-text">{{ smoothie.title }}</h2>
+        <ul class="ingredients">
+          <li v-for="(ing, index) in smoothie.ingredients" :key="index">
+            <span class="chip">{{ ing }}</span>
+          </li>
+        </ul>
+      </div>
+    </div>
   </div>
 </template>
 
