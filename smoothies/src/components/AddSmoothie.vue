5834c6774d6704be5f590cfc1e4a44be99273ab7
Jacob Bryant <jacobryas4@gmail.com>
2019-06-06 16:02:08 -0400

    added a form to add new items

--- smoothies/src/components/AddSmoothie.vue
+++ smoothies/src/components/AddSmoothie.vue
@@ -1,13 +1,17 @@
 <template>
     <div class="add-smoothie container">
         <h2 class="center-align indigo-text">Add New Smoothie Recipe</h2>
-        <form>
+        <form @submit.prevent="AddSmoothie">
             <div class="field title">
                 <label for="title">Smoothie Title</label>
                 <input type="text" name="title" v-model="title">
             </div>
             <div class="field add-ingredient">
                 <label for="add-ingredient">Add an ingredient</label>
+                <input type="text" name="add-ingredient">
+            </div>
+            <div class="field center-align">
+                <button class="btn pink">Add Smoothie</button>
             </div>
         </form>
     </div>
@@ -20,6 +24,30 @@ export default {
         return {
             title: null
         }
+    },
+    methods: {
+        AddSmoothie() {
+            console.log(this.title)
+        }
     }
 }
 </script>
+
+<style>
+
+.add-smoothie {
+    margin-top: 60px;
+    padding: 20px;
+    max-width: 500px;
+
+}
+.add-smoothie h2 {
+    font-size: 2em;
+    margin: 20px auto;
+}
+
+.add-smoothie .field {
+    margin: 20px auto;
+}
+
+</style>
